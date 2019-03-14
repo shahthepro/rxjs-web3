9f291e891e635346b74af9af82fb60d542badfe6
Shah <shahthepro@outlook.com>
2019-03-14 20:19:51 +0530

    Setup web3 provider

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+require('dotenv').config()
+
 const { Observable } = require('rxjs');
 const Web3 = require('web3');
+
+const web3 = new Web3(process.env.WEB3_PROVIDER);
+
+web3.eth.getBlock(0)
+  .then(block => {
+    console.log(`Got block ${block.hash}`)
+  })
 
 console.log('Hello!!!')
