9fdb12b173ff6c7dbb38d90f01bc82842dca35c0
Shah <shahthepro@outlook.com>
2019-03-14 21:24:00 +0530

    Concat recent 10 blocks before subscribing to new blocks

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
 require('dotenv').config()
 
-const { Observable } = require('rxjs');
+const { Observable, ReplaySubject, concat } = require('rxjs');
 const { scan } = require('rxjs/operators');
 const Web3 = require('web3');
 
 const web3 = new Web3(process.env.WEB3_PROVIDER);
 
-const blockStream = new Observable(function (observer) {
+const minedBlockStream = new Observable(async observer => {
+  try {
+    const latestBlockNumber = await web3.eth.getBlockNumber();
+    
+    for (let i = 0; i < 10; i++) {
+      let block = await web3.eth.getBlock(latestBlockNumber - 9 + i, true);
+      observer.next(block);
+    }
+
+    observer.complete();
+  
+  } catch (e) {
+    observer.error(e);
+  }
+})
+
+const newBlockStream = new Observable(observer => {
   web3.eth.subscribe('newBlockHeaders')
     .on('data', block => observer.next(block))
     .on('error', err => observer.error(err))
 });
 
-let subscriber = blockStream
+const replayNewBlocksSubject = new ReplaySubject(10);
+newBlockStream.subscribe(replayNewBlocksSubject);
+
+const recentBlocksStream = concat(minedBlockStream, replayNewBlocksSubject)
   .pipe(
     scan((blocks, newBlock) => [...blocks, newBlock].slice(-10), [])
-  )
-  .subscribe({
-    next(recentBlocks) {
-      const latestBlock = recentBlocks[recentBlocks.length - 1];
-      console.log(recentBlocks.length)
-      console.log(latestBlock.number, latestBlock.hash)
-    }
-  })
+  );
+
+recentBlocksStream.subscribe({
+  next(recentBlocks) {
+    const latestBlock = recentBlocks[recentBlocks.length - 1];
+    console.log(recentBlocks.length)
+    console.log(latestBlock.number, latestBlock.hash)
+  }
+})
