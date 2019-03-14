827cd97d2e74a57e6da38ff0e9178cea98049584
Shah <shahthepro@outlook.com>
2019-03-14 20:46:17 +0530

    Scan only the recent 10

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const { Observable } = require('rxjs');
+const { scan } = require('rxjs/operators');
 const Web3 = require('web3');
 
 const web3 = new Web3(process.env.WEB3_PROVIDER);
@@ -11,8 +12,14 @@ const blockStream = new Observable(function (observer) {
     .on('error', err => observer.error(err))
 });
 
-let subscriber = blockStream.subscribe({
-  next(block) {
-    console.log(block.number, block.hash)
-  }
-})
+let subscriber = blockStream
+  .pipe(
+    scan((blocks, newBlock) => [...blocks, newBlock].slice(-10), [])
+  )
+  .subscribe({
+    next(recentBlocks) {
+      const latestBlock = recentBlocks[recentBlocks.length - 1];
+      console.log(recentBlocks.length)
+      console.log(latestBlock.number, latestBlock.hash)
+    }
+  })
